2a2b31f6c3db68ca1ed4cef07d693b092bc65cad
aAxiong <879628926@qq.com>
2017-11-20 17:38:59 +0800

    todo
    todo

---
+++ src/components/layouts.vue
@@ -0,0 +1,24 @@
+<template>
+<section class="container">
+    <section class="menu">
+        <menus></menus>
+    </section>
+    <section class="content-container">
+        <todo></todo>
+    </section>
+</section>
+</template>
+
+<script type="text/ecmascript-6">
+import menus from './menus'
+import todo from './todo'
+export default {
+    components:{
+        menus,todo
+    }
+}
+</script>
+
+<style lang="less">
+@import '../common/style/layouts';
+</style>
